cb8bbc2f06041e8a6edad5374ea4fa30b01c2f73
Anuj <anujjain15.aj@gmail.com>
2022-11-09 16:44:03 +0530

    First Commit

---
+++ FirstApp/src/MainApp.java
@@ -0,0 +1,41 @@
+
+public class MainApp {
+
+	public static void main(String[] args) {
+
+		/*
+		 * long num = 987654321123548583L; System.out.println("Hello All");;;;;;;
+		 * System.out.println(num);
+		 * 
+		 * int a,b; a=5; b=3; int res = (a>b) ? a : b ; System.out.println(res);
+		 */
+//		int n = 5;
+//		
+//		for(int i=0;i<n;i++) 
+//		{
+//			for(int j=0;j<n;j++)
+//			
+//			{
+//				if(  i==0 || j == (n-1)/2 ||  i == n-1 )
+//				{
+//					System.out.print("*");
+//				}
+//				else
+//				{
+//					System.out.print(" ");
+//				}
+//			}
+//			System.out.println();
+//		}
+//			String s1 = new String("Nitin");
+//			String s2 = "Nitin";
+//				
+//			System.out.println("Value is: "+ s1==s2);
+//		String s1 = "a";
+//		int num = s1.charAt(0);
+//		System.out.println(num);
+		System.out.println("a==A");
+			
+			
+	}
+}
